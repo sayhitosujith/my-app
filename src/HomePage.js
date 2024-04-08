702539f375c75bdfe752a03e2eaa0ad8df157abd
sujith.s <sujith.s@ringcentral.com>
2024-04-08 18:50:01 +0530

    Updated images

--- src/HomePage.js
+++ src/HomePage.js
@@ -55,31 +55,9 @@ const CardItem = ({item}) => (
 function Welcome() {
   return (
     <div className="p-10">
-            <br></br>
-            <Breadcrumbs>
-      <a href="#" className="opacity-60">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-        </svg>
-      </a>
-
-      <a href="#" className="opacity-60">
-      <a href="#">Profile</a>
-      </a>
-      <a href="#">HomePage</a>
-    </Breadcrumbs>
+              <img style={{width: '15%', height: '15vh'}} src="https://mms.businesswire.com/media/20191209005536/en/761245/23/FoodMaven-Logo.jpg" />          
     <Avatar src="https://fellows.ias.ac.in/public/images/stock/avatar.svg?v=105894425" alt="avatar"size="xxl" div style={{float: 'right'}}></Avatar>
     <Typography variant="h2" color="Black">
-      <br></br>
-      <img style={{width: '5%', height: '10vh'}} src="https://logowik.com/content/uploads/images/maven-apache3537.jpg" />
-
-
-
       <Typography variant="small" className="mt-8 flex flex justify-center items-center">
            <Typography
                 as="a"
@@ -114,6 +92,7 @@ function Welcome() {
       <Avatar
       size="xxl"
       alt="avatar"
+      variant="rounded"
       src="https://www.kitchenherald.com/wp-content/uploads/2023/10/Virat-1.png"
       className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
     />
@@ -121,6 +100,7 @@ function Welcome() {
      <Avatar
       size="xxl"
       alt="avatar"
+      variant="rounded"
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCtZ3TP29Od5dmWVgLi3KGGbiR9dBS12NVnzhRQeTDg&s"
       className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
     />
@@ -129,6 +109,7 @@ function Welcome() {
      <Avatar
       size="xxl"
       alt="avatar"
+      variant="rounded"
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTSIdXHY0RfTWs3eo705sFof5SL4RJy9pvOyyb0L_oEA&s"
       className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
     />
@@ -137,6 +118,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJjyDtZJPb5c2S42BIohdtt7NxJ--UafzIZIgNQns18A&s"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -145,6 +127,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://artude.com/logos/logo-akshayas.jpg"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -153,6 +136,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTANzRPr2aZNqLevUOcIonhHoEAcwWH19nWc5GoafQm1A&s"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -161,6 +145,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://idealdesigns.in/wp-content/uploads/2020/08/best-hotels-logo-design-in-hyderabad-hotel-branding-bangalore-best-hotel-in-India-kaveri.jpg"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -169,6 +154,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://idealbranding.in/wp-content/uploads/2018/04/restaurant-logo-design-hotel-shubham-palace2.jpg"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -177,6 +163,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://www.tajhotels.com/content/dam/tajhotels/icons/style-icons/logo--Taj.png"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -185,6 +172,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://i.pinimg.com/originals/f6/f1/bc/f6f1bccf8d36edee548acb2c90db5e14.jpg"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -193,6 +181,7 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
+ variant="rounded"
  src="https://graphicdesigneye.com/images/hotel-logo-design-service.png"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
@@ -201,24 +190,13 @@ function Welcome() {
 <Avatar
  size="xxl"
  alt="avatar"
- src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCtZ3TP29Od5dmWVgLi3KGGbiR9dBS12NVnzhRQeTDg&s"
+ variant="rounded"
+ src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTERlpItS51IO_Y8UBZ0aXQTeq3Drrjf-scnz_LeZzZKg&s"
  className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
 />
 <b>              </b>  
 
-<Avatar
- size="xxl"
- alt="avatar"
- src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTSIdXHY0RfTWs3eo705sFof5SL4RJy9pvOyyb0L_oEA&s"
- className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
-/>
-               <b>            </b> 
-      <Avatar
-      size="xxl"
-      alt="avatar"
-      src="https://lh3.googleusercontent.com/KZdcDokR9jQgQk7FGh_z9vueezoeBWESSAK0U5r494Sav4WGp_ObBL8T_vzICe3k-rcZz5OAzuPT9XWslotw4xReGlNSzjONORvxob4"
-      className="border border-green-500 shadow-xl shadow-green-900/20 ring-4 ring-green-500/30"
-    />
+
       </Typography>
     </div>
 
@@ -241,7 +219,7 @@ function Welcome() {
         <Card className="w-98">
           <CardHeader
             variant="gradient"
-            color="orange"
+            color="white"
             className="mb-4 grid h-28 place-items-center">
          <b>One Commune - Virat Kohli</b>
           </CardHeader>
@@ -249,14 +227,11 @@ function Welcome() {
          </CardBody>
           <CardFooter className="pt-0">
           <img style={{width: '100%', height: '50vh'}} src="https://www.kitchenherald.com/wp-content/uploads/2023/10/Virat-1.png" />
-
             <Typography
           variant="p"
           sx={{ letterSpacing: '1.5px', marginLeft: '4px'  }}>
-        </Typography>
-         
-          </CardFooter>
-          
+        </Typography>     
+          </CardFooter>  
         <Typography variant="small" className="mt-6 flex justify-center">
         <b>Celebrated cricketer Virat Kohli is a culinary connoisseur at heart—but you knew that already. Sepia-toned pictures from his childhood regularly make the rounds on online fanclubs, capturing the twinkle in his eyes as he gingerly lifts a burger or crunches down on potato chips. But perhaps what the photos fail to convey at first glance is that food is more than just a passion for the Delhi boy—it is a ritual. Today, he credits the simple joys of coming home to a steaming plate of his mother’s rajma chawal or bingeing on chole bathure in the labyrinthine lanes of Delhi as a formative memory of growing up. His childhood coach, meanwhile, speaks fondly of the time when the young trainee would implore him to leave his lunch behind so they could instead feast on the biryani and kebabs he’d 
             brought from home. While Kohli would go on to achieve immense success and proclaim endless laurels in the world of cricket, it is perhaps this love for bringing people together over food that serves as the raw, beating heart of his culinary venture, one8 Commune.</b>
@@ -268,16 +243,15 @@ function Welcome() {
                 className="ml-1 font-bold">
               </Typography>
            </Typography>
-
         </Card>
- 
  <br></br>
 
+ <br></br>
 
         <Card className="w-98">
           <CardHeader
             variant="gradient"
-            color="orange"
+            color="white"
             className="mb-4 grid h-28 place-items-center">
          <b>Rameshwaram cafe</b>
           </CardHeader>
@@ -291,10 +265,13 @@ function Welcome() {
         </Typography>
           </CardFooter>
         <Typography variant="small" className="mt-6 flex justify-center">
+        <br></br>
         <b>The Rameshwaram Cafe was founded by CA Divya Raghavendra Rao and Raghavendra Rao in 2021.
 When asked about the cafe’s origin story, Divya told us that from her time in IIM Ahmedabad as a postgraduate student, she envisioned bringing traditional South Indian cuisine to the limelight with consistent quality and service; the same way global chain restaurants like KFC and McDonalds were able to.
 This dream was furthered when she met Raghavendra, who had a humble food cart in Seshadripuram and brought along with him, 15+ years of experience in the food industry.  
 Together, they launched The Rameshwaram Cafe; a name chosen to pay tribute to the late Mr. Dr. APJ Abdul Kalam’s birthplace -</b>
+<br></br>
+
            <Typography
                 as="a"
                 href="#signup"
