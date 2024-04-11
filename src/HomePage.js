ea15b2ac438493af995e4726962ccb028cc62385
sujith.s <sujith.s@ringcentral.com>
2024-04-11 18:35:40 +0530

    Added online,offline,DND

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,7 +15,10 @@ import {
   Avatar,
   data,
   Input,
-  Checkbox
+  Checkbox,
+  Popover,
+  PopoverHandler,
+  PopoverContent,
 
 } from "@material-tailwind/react";
 
@@ -76,7 +79,6 @@ function Welcome() {
         </Typography>
  
       <img style={{width: '100%', height: '65vh'}} src="https://kyari.co/cdn/shop/files/HP_Desktop_option_2-01.png?v=1708169023" />
-
        
      <br></br> 
 
@@ -205,13 +207,52 @@ function Welcome() {
     <br></br>
 
     <b><hr class="separator" /></b>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    <Popover placement="bottom">
+    News Letter
+<br></br>
+      <PopoverHandler>
+        <Button>Subscribe </Button>
+      </PopoverHandler>
+      <PopoverContent className="w-96">
+        <Typography variant="h6" color="blue-gray" className="mb-6">
+          Newsletter Subscription
+        </Typography>
+        <Typography
+          variant="small"
+          color="blue-gray"
+          className="mb-1 font-bold"
+        >
+          Your Name
+        </Typography>
+        <div className="flex gap-2">
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Button variant="gradient" className="flex-shrink-0">
+            Subscribe 
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+    <br></br>
+    <br></br>
+    <br></br>
+    <b><hr class="separator" /></b>
     <div className='flex justify-center items-center'>
        <Typography variant="h2" color="Black">
       <br></br>
      Customer Success Stories 
    <br></br>
    <br></br>
-
 
 
      <div className="flex flex-row gap-5">
--- src/MyCart.js
+++ src/MyCart.js
@@ -28,7 +28,7 @@ function MyCart() {
       <div className='w-1/2 h-screen flex items-center fullWidth justify-center'>
         
         <Card className="w-98">
-
+        <h1 style={{ color: 'Green' }}><b><div>BILL DETAILS</div></b></h1>
 
           <b><hr class="separator" /></b>
 
@@ -37,23 +37,7 @@ function MyCart() {
           <Avatar src="https://lh3.googleusercontent.com/NU4IkpoLvslBNf-uQLkOlDoMo8382HeTXOp5U6uw8kO2LWFORZE-QlvqBuscORT9leTX47dfXJuy-uod7k6Fz5GNhth75QqKd9H0pRII=h450-rw" alt="avatar" size="xxl" variant="square"/>
           <h1 style={{ color: 'red' }}><b><div>Out of Stock </div></b></h1>
           <Checkbox label="Select Meal" />
-          <Button variant="outlined" className="flex items-center gap-3">
-        Refresh
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-      </Button>
+        
           <b><hr class="separator" /></b>
           </CardBody>
           <CardFooter className="pt-0">
@@ -75,23 +59,7 @@ function MyCart() {
           <Avatar src="https://tse3.mm.bing.net/th?id=OIP.yORsSgJVQYcn7xIZ3Aa12AHaFj&pid=Api&P=0&h=220" alt="avatar" size="xxl" variant="square"/>
           <h1 style={{ color: 'Green' }}><b><div>In Stock </div></b></h1>
           <Checkbox label="Select Meal" />
-          <Button variant="outlined" className="flex items-center gap-3">
-        Refresh
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-      </Button>
+         
           <b><hr class="separator" /></b>
           </CardBody>
           <CardFooter className="pt-0">
@@ -125,20 +93,28 @@ function MyCart() {
       <card >
         <CardHeader >
             <CardBody>
-            <b><hr class="separator" /></b>
+
+            <b><hr class="w-1/2 flex items-center justify-center" /></b>
             <div style={{ width: '38rem' }}>
-            <h1 style={{ color: 'Green' }}><b><div>PRICE DETAILS</div></b></h1>
+            <h1 style={{ color: 'Orange' }}><b><div>Review your order and address details to avoid cancellations</div></b></h1>
     <b><hr class="separator" /></b>
 <br></br>
-<div>Price(2 Items):300 </div>
+<div>Item Total(2 Items):300 </div>
+<br></br>
+<b><hr class="separator"/></b>
+<br></br>
+<div>Delivery Tip :<h1 style={{ color: 'orange' }}><b><div>Add Tip</div></b></h1>
+</div>
 <br></br>
 <div>Discount : 50/-</div>
 <br></br>
-<div>Delivery Charges: FREE</div>
+<div>Delivery Charges: Free/-</div>
+<br></br>
+<div>GST and Restaurant Charges: 50/-</div>
 <br></br>
 <b><hr class="separator" /></b>
 <br></br>
-<b><div>Total Amount: 150/-</div></b>
+<b><div>To Pay: 350/-</div></b>
 <br></br>
 <b><hr class="separator" /></b>
 <br></br>
@@ -159,10 +135,8 @@ function MyCart() {
         </CardHeader>
       </card>
 
-  
-
     </div>
-   
+    
 
   );
 
