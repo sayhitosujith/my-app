d1b95562ef4618c5208c56e272cd8fb88522c4db
sujith.s <sujith.s@ringcentral.com>
2024-04-14 21:54:42 +0530

    Addeed footer with valid name

--- src/BuyNow.js
+++ src/BuyNow.js
@@ -144,7 +144,7 @@ function BuyNow() {
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <Typography variant="h5" className="mb-6">
-            Material Tailwind
+            Food Maven
           </Typography>
           <div className="grid grid-cols-3 justify-between gap-4">
             {LINKS.map(({ title, items }) => (
@@ -177,7 +177,7 @@ function BuyNow() {
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
-            &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+            &copy; {currentYear} <a href="https://material-tailwind.com/">Food Maven</a>. All
             Rights Reserved.
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
