ba6bd8515a016135fea602f8a8eacec9fc0c6ad2
sujith.s <sujith.s@ringcentral.com>
2024-04-19 01:16:39 +0530

    Added addmeal

--- src/AddMeal.js
+++ src/AddMeal.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Avatar } from "@material-tailwind/react";
-import { useCountries } from "use-react-countries";
 
 
 import {
@@ -92,7 +91,9 @@ function AddMeal() {
             </Button>
             <br></br>
             <Button variant="gradient" fullWidth>
-             Cancel
+            <a href="/">
+              Cancel
+               </a> 
             </Button>
             <Typography
           variant="p"
