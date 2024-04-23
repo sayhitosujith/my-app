fa695dd0fac65690da13067353bbeb47f951d674
sujith.s <sujith.s@ringcentral.com>
2024-04-23 22:08:13 +0530

    added breadcrumb

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,7 +15,8 @@ import {
   PopoverContent,
   Popover,
   Select,
-  Option
+  Option,
+  Breadcrumbs
 
 } from "@material-tailwind/react";
 const LINKS = [
@@ -79,6 +80,26 @@ function Welcome() {
                 className="ml-1 font-bold">
               </Typography>
            </Typography>
+           <Breadcrumbs>
+      <a href="#" className="opacity-60">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+        </svg>
+      </a>
+      <a href="#" className="opacity-60">
+      <a href="#">
+      <a href="/Welcome">
+             Welcome 
+               </a>    
+      </a>
+      </a>
+      <a href="#">HomePage</a>
+    </Breadcrumbs>
 
     <div style={{float: 'right'}}>
 
@@ -87,14 +108,12 @@ function Welcome() {
         
         <Option>    
         </Option>
-    <Badge content="6" color="white">
     <Button>
     <a href="/MyCart">
              MyCart 
                </a> 
       
     </Button>
-    </Badge>
        <Option>
        <a href="/HomePage">
              About 
@@ -112,8 +131,6 @@ function Welcome() {
           </Option></button>
       </Select>
     </div>
-  
-
             </div>
         </Typography>
  
