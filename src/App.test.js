d6d385e2572a8e0ec9455deb3db62da5fb8ca42f
sayhitosujith <sayhitosujith@gmail.com>
2025-11-21 20:00:45 +0530

    updated unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+// src/App.test.js
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ADMIN heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/ADMIN/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders email input', () => {
+  render(<App />);
+  const emailInput = screen.getByPlaceholderText(/Enter Email Address/i);
+  expect(emailInput).toBeInTheDocument();
+});
+
+test('renders password input', () => {
+  render(<App />);
+  const passwordInput = screen.getByPlaceholderText(/Enter Password/i);
+  expect(passwordInput).toBeInTheDocument();
+});
+
+test('renders practice city select', () => {
+  render(<App />);
+  const selectElement = screen.getByLabelText(/Select Practice City/i);
+  expect(selectElement).toBeInTheDocument();
 });
