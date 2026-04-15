2b9ad05efb1f234da299418601cde293ca76af79
sayhitosujith <sayhitosujith@gmail.com>
2026-04-15 18:57:51 +0530

    changes

--- .history/src/SuperAdmin_20260415185734.js
+++ .history/src/SuperAdmin_20260415185734.js
@@ -93,6 +93,7 @@ export default function SuperAdmin({
 
     window.dispatchEvent(new Event("storage"));
   };
+  /* ---------------- USERS ---------------- */
 
   const [appointments, setAppointments] = useState([]);
 
